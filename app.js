25ebe6b492eab5f93b79212a6d53146d804e47ed
Frédéric Coudert <frederic.coudert@gmail.com>
2017-10-12 13:22:57 +0200

    view

--- app.js
+++ app.js
@@ -27,11 +27,14 @@ app.model = (function () {
     data.size.height = 12;
     data.colors = [];
     data.pixels = [];
-    for (i = 0; i < D.size.width; i++) {
-        for (j = 0; j < D.size.height; j++) {
-            data.pixels[n].x=i;
-            data.pixels[n].y=j;
-            data.pixels[n].color="#CCC";
+    var n = 0;
+    for (i = 0; i < data.size.width; i++) {
+        for (j = 0; j < data.size.height; j++) {
+            data.pixels[n] = {};
+            data.pixels[n].x = i;
+            data.pixels[n].y = j;
+            data.pixels[n].color = "#CCC";
+            n++;
         }
     }
 
@@ -54,27 +57,31 @@ app.model = (function () {
 
 app.view = (function () {
 
-            function drawSquare(s) {
-                var html = '';
-                html += '<span style="margin:1px;width:20px;height:20px;background-color:' + s.color + '"></span>';
-                return (html);
+    function drawSquare(s) {
+        var html = '';
+        html += '<span style="margin:1px;width:20px;height:20px;background-color:' + s.color + '"></span>';
+        return (html);
 
-            }
+    }
 
 
-            return {
-                draw: function (D) {
-                    var html='';
-                    for (i = 0; i < D.size.height; i++) {
-                        for (j = 0; j < D.size.width; j++) {
-                            D.pixels.forEach(function (pixel) {
-                                if (pixel.x === j && pixel.y === i) html += drawSquare(pixel);
-                            });
-                        }
-
-                    }
-                $('#tableau').html(html);    
+    return {
+        draw: function (D) {
+            console.log('draw asked...');
+            console.log(D);
+            var html = '';
+            for (i = 0; i < D.size.height; i++) {
+                for (j = 0; j < D.size.width; j++) {
+                    D.pixels.forEach(function (pixel) {
+                        console.log(pixel);
+                        if (pixel.x === j && pixel.y === i) html += drawSquare(pixel);
+                    });
                 }
-            };
+                html += '<br>';
+            }
+            console.log(html);
+            $('#tableau').html(html);
+        }
+    };
 
-            })();
+})();
