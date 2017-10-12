785628dbcf8a8b0a3e4d105ff5ccaa0fb92c8aec
Frédéric Coudert <frederic.coudert@gmail.com>
2017-10-12 12:58:28 +0200

    yo

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 app = (function () {
 
     return {
-        init:function () {
+        init: function () {
             console.log('init app...');
-            $('#valid-size').on('click',function() {
-                app.model.changeSize($('#x-val').val(),$('#y-val').val());
+            $('#valid-size').on('click', function () {
+                app.model.changeSize($('#x-val').val(), $('#y-val').val());
                 app.view.draw(app.model.getData());
             });
 
@@ -12,7 +12,35 @@ app = (function () {
 
 
         }
+    };
+
+
+
+})();
+
+
+app.model = (function () {
+
+    var data = {};
+    data.size = {};
+    data.size.width = 12;
+    data.size.height = 12;
+    data.colors = [];
+    data.pixels = [];
+
+
+    return {
+        changeSize(x, y) {
+            data.size.width = parseInt(x, 10);
+            data.size.height = parseInt(y, 10);
+        },
+        getData: function () {
+            return (data);
+        }
+
     }
+
+
 
 
 
